5033309359976ba9256f1f2e37621193484698c8
Sajid Ispak Choudhury <91846733+0-94@users.noreply.github.com>
2021-10-26 02:39:31 +0600

    first commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+package com.Sajid;
+
+public class Main {
+
+
+
+        public static void main(String[] args){
+            Student s1 = new Student();
+            Student s2 = new Student("Sajid");
+            Student s3 = new Student(2012020094);
+            s1.display();
+    } /* Name   : Sajid Ispak Choudhury
+   ID     : 2012020094
+   Section: B
+   Email  : cse_2012020094@lus.ac.bd
+   Date   : 09-08-2021
+ */
+}
---
+++ Student.java
@@ -0,0 +1,38 @@
+package com.Sajid;
+
+public class Student {
+    private String name;
+    private int id ;
+    private static String universityName ;
+
+
+   public Student(){
+       System.out.println("Default constructor");
+       name = "Sajid"
+       id = 2012020094;
+
+       public Student (String name , int id)
+       {
+          this. name = name;
+          this. id = id;
+           System.out.println("Name : " + name);
+           System.out.println("Id : " + id);
+       }
+       void display()
+       {
+           System.out.println("University name : "+ universityName);
+       }
+
+
+       /* Name   : Sajid Ispak Choudhury
+   ID     : 2012020094
+   Section: B
+   Email  : cse_2012020094@lus.ac.bd
+   Date   : 09-08-2021
+ */
+
+
+
+    }
+
+}
